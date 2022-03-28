7038abfc2c9dc15b63b36c4a4effba946229a2fe
alejandra villamil <kahome.92@gmail.com>
2022-03-27 20:43:59 -0500

    diplomado

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+$(".padre").mouseover(function(){
+    $(".padre").css("background","Gray")
+})
+$(".menu").css("color", "blue")
+$(".evento1").click( function(){
+$(".evento1").css("font-size", "30px")
+})
---
+++ javascript/script.js
@@ -0,0 +1,53 @@
+$(".pais1").css("color", "red")
+$(".evento1").dblclick(function(){
+    $(".evento1").css("background","yellow")
+})
+$(".pais2").css("color", "blue")
+$(".evento2").click( function(){
+$(".evento2").css("font-size", "20px")
+})
+$(".pais3").css("color", "pink")
+$(".evento3").dblclick(function(){
+    $(".evento3").css("background","Brown")
+})
+$(".pais4").css("color", "purple")
+$(".evento4").dblclick(function(){
+    $(".evento4").css("font-size","30px")
+})
+$(".pais5").css("color","Turquoise")
+$(".evento5").click( function(){
+    $(".evento5").css("font-size", "40px")
+})
+$(".pais6").css("color", "green")
+$(".evento6").dblclick(function(){
+    $(".evento6").css("background","Fuchsia")
+})
+$(".pais7").css("color", "orange")
+$(".evento7").click( function(){
+$(".evento7").css("font-size", "30px")
+})
+$(".pais8").css("color", "blue")
+$(".evento8").click( function(){
+$(".evento8").css("font-size", "20px")
+})
+$(".pais9").css("color", "Gray")
+$(".evento9").dblclick(function(){
+    $(".evento9").css("background","Pink")
+})
+$(".pais10").css("color", "Purple")
+$(".evento10").click( function(){
+$(".evento10").css("font-size", "30px")
+})
+$(".caja1").mouseover(function(){
+    $(".caja1").css("background","orange")
+})
+$(".caja2").mouseover(function(){
+    $(".caja2").css("background","Pink")
+})
+$(".caja3").mouseover(function(){
+    $(".caja3").css("background","green")
+})
+$(".menu").css("color", "blue")
+$(".evento1").click( function(){
+$(".evento1").css("font-size", "20px")
+})
